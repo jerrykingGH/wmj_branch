6315680286d0f98306fea753d58e680cd53c25fa
jerrykingGH <41789638+jerrykingGH@users.noreply.github.com>
2022-05-06 10:17:08 +0800

    add book config excel libs proto

---
+++ Server/Hotfix/Config/ConfigComponent_SetConfigLoader_Awake.cs
@@ -0,0 +1,10 @@
+namespace ET
+{
+    public class ConfigComponent_SetConfigLoader_Awake: AwakeSystem<ConfigComponent>
+    {
+        public override void AwakeAsync(ConfigComponent self)
+        {
+            self.ConfigLoader = new ConfigLoader();
+        }
+    }
+}
